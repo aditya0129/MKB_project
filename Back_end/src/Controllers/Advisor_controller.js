6cf62e54dddf93ae5e9464088b51fbed04af8549
Aditya Prajapati <adityakumar00129gmail.com>
2024-07-24 12:44:51 +0530

    image api done

--- Back_end/src/Controllers/Advisor_controller.js
+++ Back_end/src/Controllers/Advisor_controller.js
@@ -186,7 +186,7 @@ const Advisor_register = async function (req, res) {
       State,
       DOB,
       Age,
-      Image:  "image/" + req.file.filename,
+      Image:  "images/" + req.file.filename,
     });
     await newUser.save();
     res.status(201).json({ status: true, user: newUser });
